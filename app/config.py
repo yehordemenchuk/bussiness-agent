f7fa4efb30c058689a80125208909d1bfaaa1373
Yehor Demenchuk <demenchuk1210m@gmail.com>
2026-07-11 21:54:43 +0200

    fix system prompt

--- app/config.py
+++ app/config.py
@@ -11,7 +11,7 @@ class Config:
 
     OPENAI_API_BASE = "https://api.openai.com/v1"
 
-    LANGCHAIN_API_KEY = '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'
+    LANGCHAIN_API_KEY =
 
     LANGCHAIN_PROJECT = 'business-ai-service'
 
@@ -19,15 +19,15 @@ class Config:
 
     LANGCHAIN_TRACING_V2 = "true"
 
-    HUBSPOT_API_KEY = 'pat-eu1-9e3141b7-23cf-4c5a-bfcb-42ecd8ebf5bc'
+    HUBSPOT_API_KEY =
 
-    SLACK_API_TOKEN = 'xoxb-11460652193702-11456415326739-DEdFB3n68PS6qwS8de86e71c'
+    SLACK_API_TOKEN =
 
     REST_ADDRESS = "http://localhost"
 
     EMBEDDINGS_MODEL_NAME = "all-MiniLM-L6-v2"
 
-    TAVILY_API_KEY = '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'
+    TAVILY_API_KEY =
 
     SYSTEM_PROMPT_PATH = os.path.join("prompt", "system_prompt.txt")
 
